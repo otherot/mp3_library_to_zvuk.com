28b87297f77b41324b5ca6d4bed4c8f4aed6770c
otherot <otherot@gmail.com>
2026-03-26 13:36:27 +0300

    feat: add torrent search models v0.2.0

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,8 @@
 """
 MP3 Library to Zvuk.com Comparator
 
-Сравнение локальной MP3-библиотеки с библиотекой на zvuk.com
+Compare your local MP3 library with zvuk.com collection
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "MP3 Library Comparator Team"
